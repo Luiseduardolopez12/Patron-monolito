977a0f7d55e2da84735b07f49210cff2c3bde9e2
Luis <luigieduardo1212@gmail.com>
2026-08-05 21:44:08 -0600

    Update Program.cs

--- BibliotecaMonolito/Program.cs
+++ BibliotecaMonolito/Program.cs
@@ -3,32 +3,31 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//registro de servicio de contexto de base de datos
-builder.Services.AddDbContext<LibraryDbContext>(options => 
-options.UseSqlite(builder.Configuration.GetConnectionString("Default")
-?? "Data Source=biblioteca.db"));
-
+// adartar servicios al contenedor.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=biblioteca.db"));
 
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-//aplicar migraciones automaticamente
+
+//aplicar migraciones pendientes al iniciar la aplicación
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
     db.Database.Migrate();
+
 }
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
